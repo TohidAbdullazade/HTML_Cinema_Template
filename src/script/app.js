3a7f9916d7deb5dfceae77d793420829dbdddf10
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-04-21 21:08:06 +0400

    I create html Cinema Template

---
+++ src/script/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", runAllFunction);
+
+// c Hamburg Menu Function
+let HamburgMenuObject = {
+  mobileMenu: document.querySelector(".mobile-menu"),
+  menuButton: document.querySelector(".fa-bars"),
+
+  showHamburgMenu: function () {
+    this.menuButton.addEventListener("click", (e) => {
+      if (e.target) {
+        this.mobileMenu.classList.toggle("show-mobile-menu");
+      }
+    });
+  },
+};
+
+//c Output of all methods
+function runAllFunction(e) {
+  if (e.target) {
+    HamburgMenuObject.showHamburgMenu();
+  } else {
+    throw new Error("There is an error occured when DOM Load");
+  }
+}
